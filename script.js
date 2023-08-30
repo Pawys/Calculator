64bc7dc109a3d46afe990d3ddc5fc1d5515310af
Pawys <88272261+Pawys@users.noreply.github.com>
2023-08-30 18:30:56 +0200

    Add Working Display

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let a;
 let b;
 let operator;
+let buttons = document.querySelectorAll("button");
+let display = document.querySelector(".display")
+let displayValue = display.textContent;
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    displayValue = displayValue + button.textContent;
+    display.textContent = displayValue;
+  })
+}
+
+function operate(a, b, operator){
+  add(a, b)
+}
 function add(a, b)
 {
 return a + b;
