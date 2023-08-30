f00d989c60e43fe1115fc1a65c5c6a0998a5517a
Pawys <88272261+Pawys@users.noreply.github.com>
2023-08-30 18:08:25 +0200

    Add Basic Functions

---
+++ script.js
@@ -0,0 +1,16 @@
+let a;
+let b;
+let operator;
+function add(a, b)
+{
+return a + b;
+}
+function subtract(a, b) {
+return a - b; 
+}
+function multiply(a, b) {
+return a * b; 
+}
+function divide(a, b) {
+return a / b; 
+}
