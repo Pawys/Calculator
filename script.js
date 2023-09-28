d1316bba89e3a47e3c2b6b27ce9aa1744fbb4d2a
Pawys <88272261+Pawys@users.noreply.github.com>
2023-09-28 17:59:00 +0200

    Optimize Code

--- script.js
+++ script.js
@@ -1,131 +1,150 @@
+const buttons = document.querySelectorAll(".btn");
+const display = document.querySelector(".display")
+
 let a;
 let b;
 let operator;
-let buttons = document.querySelectorAll(".btn");
-let display = document.querySelector(".display")
-let displayValue;
-let displayArray;
-let buttonValue;
-let buttonNumValue;
-let clicked = 0;
-let error = 0;
 let btn;
+
+let buttonValue;
+let displayValue;
+
+let state = 0;
+let error = 0;
+
 for (let button of buttons) {
 button.addEventListener("click", click);
 }
+
 document.addEventListener('keydown', key)
+
 function key(key){
-  key.preventDefault();
-  for (let button of buttons) {
-    if (key.key === button.textContent) {
-      click(button);
-    } else if (key.key === "*"){
-     if (button.textContent === "x"){
-       click(button);
-     }
-    }else if(key.key === "Backspace"){
-     if (button.textContent === "⌫"){
-       click(button);
-     }
-    }else if (key.key === 'Enter'){
-       if (button.textContent === "="){
-         click(button);
-       }
-     }
+  for (let button of buttons){
+    if (key.key === button.textContent){click(button)}
+    else {
+      switch(key.key){
+        case '*': 
+          if (button.textContent === "x"){click(button)}
+          break;
+        case "Backspace":
+          if (button.textContent === "⌫"){click(button)}
+          break;
+        case "Enter":
+          if (button.textContent === "Enter"){click(button)}
+          break;
+      }
+    }
   }
 }
-// This code is the worst thing I ever wrote
+
 function click(button){
-  if(button.buttons === 0){
-  console.log(typeof(button));
+
+  if (error)clear();
+
+  if (button.buttons === 0){
     btn = button.currentTarget;
   }else {
-    console.log('chuj');
     btn = button;
   }
   buttonValue = btn.textContent;
   displayValue = display.textContent;
-  buttonNumValue = Number(buttonValue)
-  if (buttonValue === 'AC'){
+
+  switch(buttonValue){
+    default:
+      if (displayValue === ''){return;}
+      switch(state){
+        case 0:
+          state = 1;
+          operator = buttonValue;
+          a = Number(displayValue);
+          break;
+        case 1:
+          state = 0;
+          operator = '';
+          a = Number(displayValue);
+          break;
+        case 2:
+          b = Number(displayValue);
+          displayValue = equal(a,b,operator);
+          a = Number(displayValue);
+          b = '';
+          operator = buttonValue;
+          state = 1;
+          break;
+      }
+        setColor(btn, 1);
+      break;
+    case String(Number(buttonValue)):
+      if (state === 1){
+        state = 2;
+        displayValue = '';
+        for (let btn of buttons){
+          setColor(btn, 0);
+        }
+      }
+      displayValue = displayValue + buttonValue; 
+      if ((display.textContent).length > 7)return;
+      break;
+    case ".":
+      for (letter of displayValue.split('')){
+        if (letter === '.'){return};
+      }
+      if((display.textContent).length > 7){return};
+      break;
+    case "=":
+      switch(state){
+        default:
+          for (let btn of buttons){
+            setColor(btn, 0);
+          }
+          display.textContent = displayValue;
+          state = 0
+          break;
+        case 2:
+          b = Number(displayValue);
+          displayValue = equal(a,b,operator);
+          a = Number(displayValue);
+          b = '';
+          operator = '';
+          state = 0;
+          break;
+      }
+      break;
+    case "⌫":
+      backspace();
+      break;
+    case "AC":
       clear();
-      return
+      break;
   }
-  if (error === 1){return};
-  if (buttonNumValue > -1 && buttonNumValue < 11|| buttonValue === '.'){
-    if (clicked === 1){
-      clicked = 2;
-      displayValue = '';
-      for (let btn of buttons){
-      btn.classList.remove('toggled')
-      }
-    }else{
-      if (buttonValue === '.'){
-        for (letter of displayValue.split('')){
-          if (letter === '.'){return};
-        };
-      }
-    if((display.textContent).length > 7){return};
-    }
-    displayValue = displayValue + buttonValue; 
-  }else if (buttonValue === "="){
-    if (clicked !== 2){
-      for (let btn of buttons){
-      btn.classList.remove('toggled')
-      }
-      display.textContent = displayValue;
-      clicked = 0;
-    }else{
-      b = Number(displayValue);
-      displayValue = equal(a,b,operator);
-      a = Number(displayValue);
-      b = '';
-      operator = '';
-      clicked = 0;
-    }
-  }else if (buttonValue === '⌫'){
-    backspace();
-  }else{
-    if (displayValue === ''){return;}
-    if (clicked === 1){
-      clicked = 0;
-      operator = '';
-      a = Number(displayValue);
-      button.currentTarget.classList.toggle('toggled')
-    }else if (clicked === 2){
-      button.currentTarget.classList.toggle('toggled')
-      b = Number(displayValue);
-      displayValue = equal(a,b,operator);
-      a = Number(displayValue);
-      b = '';
-      operator = buttonValue;
-      clicked = 1;
-    }else {
-      clicked = 1;
-      operator = buttonValue;
-      a = Number(displayValue);
-      btn.classList.toggle('toggled')
-      return;
-    }
+display.textContent = displayValue;
+}
+function setColor(button, num){
+  if (num){
+    button.style.backgroundColor = "white";
+    button.style.color = "#f19a3a";
   }
-  display.textContent = displayValue;
+  else {
+    button.style.backgroundColor = null;
+    button.style.color = null;
+  }
 }
 function equal(){
-    let anwser = operate(a,b,operator)
-    if (Number.isInteger(anwser) !== true){
-     if (anwser.toString().split('.')[1].length > 3){
-       anwser = (Math.round((anwser + Number.EPSILON) * 1000) / 1000);
-       if (String(anwser).length > 8){
-         anwser = Math.round(anwser);
-       }
-     };
-    }
+  let anwser = operate(a,b,operator)
+  if (!Number.isInteger(anwser)){
+   if (anwser.toString().split('.')[1].length > 3){
+     anwser = (Math.round((anwser + Number.EPSILON) * 1000) / 1000);
+     if (String(anwser).length > 8){
+       anwser = Math.round(anwser);
+     }
+   };
+  }
   if (String(anwser).length > 8){
-    console.log('chuj')
     display.style.fontSize = "70px";
     anwser = "Number Too Big";
     error = 1;
   }
-    return anwser;
+  return anwser;
 }
 
 function clear(){
@@ -133,12 +152,13 @@ function clear(){
   b = '';
   operator = '';
   displayValue = '';
-  clicked = 0;
+  state = 0;
   error = 0;
   display.textContent = displayValue;
+  buttons.forEach((button) => setColor(button, 0))
 }
 function backspace(){
-  displayArray = displayValue.split('')
+  displayArray = displayValue.split('');
   displayArray.pop();
   displayValue = Number(displayArray.join(''));
   if (displayArray.join('') === ''){
@@ -148,28 +168,15 @@ function backspace(){
 }
 function operate(a, b, operator){
   if (operator === '+'){
-    return add(a, b);
+    return a + b;
   } else if (operator === '-'){
-    return subtract(a, b);
+    return a - b;
   }else if (operator === 'x'){
-    return multiply(a, b);
+    return a * b;
   }else {
     if (b == 0){
       return ":P"
     }
-    return divide(a, b);
+    return a / b;
   }
-}
-function add(a, b)
-{
-return a + b;
-}
-function subtract(a, b) {
-return a - b; 
-}
-function multiply(a, b) {
-return a * b; 
-}
-function divide(a, b) {
-return a / b; 
 }
